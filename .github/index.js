90f914b4b69d538bb9984986958549347a3e2231
Moshe Druion <moshedruion@gmail.com>
2022-06-21 16:04:08 +0300

    first commit

--- .github/index.js
+++ .github/index.js
@@ -1,6 +1,22 @@
-let X = "8";
-let Y = "21";
+let X = 8;
+let Y = fibonacci(X);
+let Z = ('The Fibonacci of ' + X + ' is ' +Y);
 
-let fibonacci = `The Fibonacci of ${X} is ${Y}`;
+document.getElementById("output").innerHTML = Z;
 
-document.getElementById("output").innerHTML = fibonacci;
+function fibonacci(X)
+{
+    let num1=0;
+    let num2=1;
+    let sum;
+    let i=0;
+    for (i = 1; i < X; i++) 
+    {
+        sum=num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+    return num2;
+}
+
+fibonacci(X);
